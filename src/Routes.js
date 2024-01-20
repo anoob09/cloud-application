b09f5c933da7c2102a0f8ebe6e3e68abad1694aa
Anup Swarnkar <aswarnkar.anup.swarnkar@gmail.com>
2024-01-21 01:18:05 +0530

    add storage page details
    add storage page details

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomeLayout from "./pages/home/HomeLayout";
 import Email from "./pages/email/Email";
 import Photo from "./pages/photo/Photo";
+import Storage from "./pages/storage/Storage";
 import NotFound from "./pages/page404/NotFound";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomeLayout />} />
         <Route path="/email" element={<Email />} />
         <Route path="/photos" element={<Photo />} />
+        <Route path="/storage" element={<Storage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       </div>
---
+++ src/pages/storage/Storage.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function Storage() {
+  return (
+    <Card sx={{ maxWidth: 600 }}>
+      <CardContent>
+        <Typography variant='h3' gutterBottom>
+          Welcome to Storage!
+        </Typography>
+        <Typography variant="h4">
+         You have 50 GB of Storage left 
+        </Typography>
+        <h1 className="ml-2 mt-8" color="text.secondary">
+          You might be running out of space!
+        </h1>
+      </CardContent>
+      <CardActions className="ml-2 mt-0" >
+        <Button size="small">Click here to upgrade to 500 GB plan</Button>
+      </CardActions>
+    </Card>
+  );
+}
